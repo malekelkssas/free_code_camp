1ce3cb1c257c7b9b16b6521974c613df7caf34ab
Malek Mohamed Noureldean Elkssas <87043730+malekelkssas@users.noreply.github.com>
2022-10-09 15:48:45 +0000

    my last try in Q-learning algorithm

--- RPS.py
+++ RPS.py
@@ -1,32 +1,39 @@
 # The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
+# try hidden markov model nexttime
+
 import numpy as np
 import myGlobal
 
+
 def player(prev_play, opponent_history=[]):
+  # print('myGlobal.EPISODES: ',myGlobal.EPISODES)
   if check(myGlobal.EPISODES):
     opponent_history = []
   if len(opponent_history)==0:
-    opponent_history.append(0)
-    return myGlobal.map[0]
+    state = 0
+    opponent_history.append(state)
+    return myGlobal.map[state]
   myGlobal.EPISODES -=1
   action=-1
   state = opponent_history[-1]  #my prev play
   reward = myGlobal.rewards[myGlobal.map[state]][prev_play]
-  if np.random.uniform(0,1)<myGlobal.epsilon :
+  if np.random.uniform(0,1)< myGlobal.epsilon or myGlobal.EPISODES >900:
     action = int(np.random.uniform(0,3))
+    next_state = action
+    myGlobal.Q[state,action] = myGlobal.Q[state,action] + 0.25* (reward + 0.25 *np.max(myGlobal.Q[next_state, :])-np.absolute(myGlobal.Q[state,action]))
   else:
     action = np.argmax(myGlobal.Q[state,:]) 
-  next_state = action
-  myGlobal.Q[state,action] = myGlobal.Q[state,action] + myGlobal.LEARNING_RATE * (reward + myGlobal.GAMMA *  np.max(myGlobal.Q[next_state, :]) - myGlobal.Q[state,action])
-  myGlobal.epsilon-=0.0001
+    next_state = action
+    myGlobal.Q[state,action] = myGlobal.Q[state,action] +   myGlobal.LEARNING_RATE * (reward + myGlobal.GAMMA *  np.max(myGlobal.Q[next_state, :]) - np.absolute(myGlobal.Q[state,action]))
   opponent_history.append(action)
+  myGlobal.epsilon-=0.0001
   return myGlobal.map[action]
 
   
 def check(E):
-  if E==0 :
+  if E==0:
     myGlobal.Q = np.zeros((myGlobal.STATES,myGlobal.ACTIONS))
-    myGlobal.epsilon = 0.9
+    myGlobal.epsilon = 0.25
     myGlobal.EPISODES= 999
     return True
   return False
--- myGlobal.py
+++ myGlobal.py
@@ -1,29 +1,12 @@
 import numpy as np
 map = {0:'R',1:'P',2:'S'}
 mapInverse = {'R':0,'P':1,'S':2}
-rewards = {'R':{'R':-2,'P':-5,'S':10},'P':{'R':10,'P':-2,'S':-5},'S':  {'R':-5,'P':10,'S':-2}}
+rewards = {'R':{'R':-3,'P':-5,'S':10},'P':{'R':10,'P':-3,'S':-5},'S':  {'R':-5,'P':10,'S':-3}}
 STATES = 3    #opponent     
 ACTIONS = 3  #player
-# LEARNING_RATE = 0.75
-# GAMMA = 0.7
-LEARNING_RATE = 0.95    # a higher rate a faster the model learn
+LEARNING_RATE = 0.7    # a higher rate a faster the model learn
 GAMMA = 0.75
 Q = np.zeros((STATES,ACTIONS))
-epsilon = 0.9
+epsilon = 0.25
 EPISODES= 999
 best = {'R':2,'P':0,'S':1}
-
-
-# map = {0:'R',1:'P',2:'S'}
-# mapInverse = {'R':0,'P':1,'S':2}
-# rewards = {'R':{'R':-3,'P':-6,'S':10},'P':{'R':10,'P':-3,'S':-6},'S':  {'R':-6,'P':10,'S':-3}}
-# STATES = 3    #opponent     
-# ACTIONS = 3  #player
-# # LEARNING_RATE = 0.75
-# # GAMMA = 0.7
-# LEARNING_RATE = 0.95    # a higher rate a faster the model learn
-# GAMMA = 0.78
-# Q = np.zeros((STATES,ACTIONS))
-# epsilon = 0.8
-# EPISODES= 999
-# best = {'R':2,'P':0,'S':1}
